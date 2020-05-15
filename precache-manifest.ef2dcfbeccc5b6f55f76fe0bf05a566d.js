f0890a932202f691c1c8bcb5a94745aebc18ab3e
Camirojasserantoni <camilarojasserantoni@gmail.com>
2020-05-15 12:15:56 -0400

    Updates

---
+++ precache-manifest.ef2dcfbeccc5b6f55f76fe0bf05a566d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978918f27558fe23a0189b5d6ae5f347",
+    "url": "/Camirojasserantoni/climaReact/index.html"
+  },
+  {
+    "revision": "8121d6285631bb40b5a0",
+    "url": "/Camirojasserantoni/climaReact/static/css/main.3751ae63.chunk.css"
+  },
+  {
+    "revision": "dce6df4bba4c2664048e",
+    "url": "/Camirojasserantoni/climaReact/static/js/2.e908e84a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Camirojasserantoni/climaReact/static/js/2.e908e84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8121d6285631bb40b5a0",
+    "url": "/Camirojasserantoni/climaReact/static/js/main.8b0adf34.chunk.js"
+  },
+  {
+    "revision": "d5fe442569a781b86127",
+    "url": "/Camirojasserantoni/climaReact/static/js/runtime-main.7520750e.js"
+  }
+]);
